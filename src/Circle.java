5e07ded9682b6738e2222372b499e70b816cfcc1
Constanza Delfino <111506427+cdelfino@users.noreply.github.com>
2023-11-27 14:40:05 -0300

    initial commit

---
+++ src/Circle.java
@@ -0,0 +1,47 @@
+public class Circle extends Shape {
+    //atributo de Circle
+    private double radius;
+
+    // constructores
+    public Circle() {
+        super();  // constructor de la clase base Shape
+        this.radius = 1.0;
+    }
+
+    public Circle(double radius) {
+        super();  // constructor de la clase Shape
+        this.radius = radius;
+    }
+
+    public Circle(double radius, String color, boolean filled) {
+        super(color, filled);
+        this.radius = radius;
+    }
+
+    // metodo específico de Circle
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+
+    // mtodo abstracto para sacar el area
+    @Override
+    public double getArea() {
+        return Math.PI * radius * radius;
+    }
+
+    // mtodo abstracto para sacar el perimetro
+    @Override
+    public double getPerimeter() {
+        return 2 * Math.PI * radius;
+    }
+
+    // metodo toString
+    @Override
+    public String toString() {
+        return "Circle [radius=" + radius + ", " + super.toString() + "]";
+    }
+}
---
+++ src/Shape.java
@@ -0,0 +1,44 @@
+public abstract class Shape {
+    // atributos
+    private String color;
+    private boolean filled;
+
+    // constructores
+    public Shape() {
+        this.color = "red";
+        this.filled = true;
+    }
+
+    public Shape(String color, boolean filled) {
+        this.color = color;
+        this.filled = filled;
+    }
+
+    // metodos
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public boolean isFilled() {
+        return filled;
+    }
+
+    public void setFilled(boolean filled) {
+        this.filled = filled;
+    }
+
+    // metodos
+    public abstract double getArea();
+
+    public abstract double getPerimeter();
+
+
+    @Override
+    public String toString() {
+        return "Shape [color=" + color + ", filled=" + filled + "]";
+    }
+}
